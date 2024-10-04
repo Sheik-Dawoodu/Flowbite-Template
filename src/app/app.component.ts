1c614e7b8cc3afa4e93a7aef9fa0e9a81323588e
Rubanrubi <ruban@sparkouttech.com>
2024-10-04 12:00:04 +0530

    flowbite dropdown issue checking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
+import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
+import { ModalComponent } from './components/shared/modal/modal.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent, ModalComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,10 @@ export const routes: Routes = [
     path: '',
     loadComponent: () => import('./components/modals/modals.component').then(m => m.ModalsComponent)
   },
+  {
+    path: 'test',
+    loadComponent: () => import('./components/auth/login/login.component').then(m => m.LoginComponent)
+  },
  
 
 ];
